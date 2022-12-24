481b7d66c2463d5460d9d4e87d79f93a1197ce2f
IgorCisowski <igorcisowski2098@gmail.com>
2022-12-24 17:51:47 +0100

    enrich App functionality

--- main.js
+++ main.js
@@ -1,136 +1,130 @@
-// enter 13
-// imported HTML elements
-const input = document.querySelector("input");
-const h2 = document.querySelector("h2");
-const ul = document.querySelector("ul");
+// HTML ELEMENTS
+const inputTodo = document.getElementById("inputTodo");
+const add = document.getElementById("div__input__button--addToDo");
+const ul = document.getElementById("storeToDo");
 const li = document.getElementsByTagName("li");
-const addToDoButton = document.getElementById("div__input__button--addToDo");
+
+// TOGGLE UTTON OR MESSAGE
+const displayDefault = document.getElementById("display-default-message");
 const deleteAll = document.getElementById("deleteAll");
-const displayDefault = document.getElementById("displayDefault");
-const editDelete = document.getElementById("editDelete");
-// save TODOs to local storage
+
+// COUNTING TODOS VARIABLES
+const active = document.querySelector("#left-todos-counter");
+const completed = document.querySelector("#completed-todos-counter");
+
+// SAVE TODOS
 const saveToDo = () => {
   localStorage.setItem("todos", JSON.stringify(todos));
 };
-// variables id, flag, todos
-let flag = false;
-let id = 1;
-let todos;
 const savedTodos = JSON.parse(localStorage.getItem("todos"));
 if (Array.isArray(savedTodos)) {
   todos = savedTodos;
 } else {
   todos = [];
 }
-// creating a todo and adding it to the list
-const create = (title) => {
-  todos.push({
-    title: title,
-    id: ++id,
-  });
+// VARIABLES
+let id = 0;
 
-  saveToDo();
-};
-// filtering throughout the todos list and removing the clicked one
-const remove = (idToDelete) => {
-  todos = todos.filter((todo) => todo.id != idToDelete);
-
-  saveToDo();
-};
-// taking the value of the input and assigning it to the object title
+// ADD
 const addToDo = () => {
-  const title = input.value;
-  if (input.value == "") {
-    alert("You need to add a new task!");
+  todos.push({
+    title: inputTodo.value,
+    id: ++id,
+    completed: false,
+  });
+  // MESSAGE THE USER if theres no value in the input
+  if (inputTodo.value == "") {
+    alert("You need fill in the input field!");
     return false;
   }
-  create(title);
   render();
+  saveToDo();
 };
-addToDoButton.addEventListener("click", addToDo);
-//add todo by button===============================================================================
-// const enter = (key) => {
-//   if (key.keyCode === "13") {
-//     create();
-//   }
-// };
+// APPEND OnCLick
+add.addEventListener("click", addToDo);
 
-// click target assigned to a vatiable so it detects the todo id
+// DELETE
 const deleteToDo = (e) => {
-  const deleteLi = e.target;
-  const idToDelete = deleteLi.id;
-
-  remove(idToDelete);
+  const clickedButtonId = e.target.id;
+  todos = todos.filter((li) => li.id != clickedButtonId);
   render();
+  saveToDo();
 };
-// function for clearing all the todos in the array
+
+// COMPLETE
+const done = (e) => {
+  const clickedCheckboxId = e.target.id;
+  todos = todos.map((li) => {
+    if (li.id == clickedCheckboxId) {
+      li.completed = !li.completed;
+    }
+    return li;
+  });
+  render();
+  saveToDo();
+};
+
+// ENTER APPEND
+inputTodo.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    addToDo();
+    e.target.blur();
+  }
+});
+
+// COUNT
+const countTasks = () => {
+  let completedTodos = todos.filter((li) => {
+    return li.completed == true;
+  });
+  active.textContent = todos.length - completedTodos.length;
+  completed.textContent = completedTodos.length;
+  saveToDo();
+};
+
+// DELETE ALL
 const clearAllToDo = () => {
   todos.splice(0, todos.length);
   render();
   saveToDo();
 };
 deleteAll.addEventListener("click", clearAllToDo);
-// =========================
-// const checkToDo = (li, checkbox) => {
-//   if (!flag) {
-//     flag = !flag;
-//     checkbox.textContent = "✓";
-//     li.style.textDecoration = "line-through";
-//     li.style.color = "#ffffffCC";
-//   } else {
-//     flag = !flag;
-//     checkbox.textContent = "";
-//     li.style.textDecoration = "none";
-//     li.style.color = "#ffffff";
-//   }
-//   saveToDo();
-// };
 
-// rendering the todos from the list on the website
+// RENDER
 const render = () => {
   ul.textContent = "";
-  todos.forEach(({ title, id }) => {
+  todos.forEach(({ title, id, completed }) => {
+    // ADD li to ul
     const li = document.createElement("li");
     li.textContent = title;
-    // EDIT TODO
-    // const edit = document.createElement("button");
-    // edit.innerHTML = `<i class="fa-regular fa-pen-to-square"></i>`;
-    // edit.classList.add("ul__li__button__editToDo");
-    // li.appendChild(edit);
-    // edit.addEventListener("click", editTodo);
-    // DELETE TODO
-    const deleteLi = document.createElement("button");
-    deleteLi.onclick = deleteToDo;
-    deleteLi.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
-    deleteLi.classList.add("ul__li__button__deleteToDo");
-    deleteLi.id = id;
-    li.appendChild(deleteLi);
-    // MARKED AS DONE TODO
-    const checkToDo = () => {
-      if (!flag) {
-        flag = !flag;
-        checkbox.textContent = "✓";
-        li.style.textDecoration = "line-through";
-        li.style.color = "#ffffffCC";
-      } else {
-        flag = !flag;
-        checkbox.textContent = "";
-        li.style.textDecoration = "none";
-        li.style.color = "#ffffff";
-      }
-      saveToDo();
-    };
-    const checkbox = document.createElement("button");
-    checkbox.onclick = checkToDo;
-    checkbox.classList.add("ul__li__button__doneToDo");
-    li.appendChild(checkbox);
-    // ------------------------------------------------------
     ul.appendChild(li);
+
+    //DELETE li from ul
+    const liDeleteButton = document.createElement("button");
+    liDeleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+    liDeleteButton.classList.add("deleteIcon");
+    li.appendChild(liDeleteButton);
+
+    liDeleteButton.id = id;
+    liDeleteButton.onclick = deleteToDo;
+
+    // // MARK li as done
+    const checkbox = document.createElement("input");
+    li.appendChild(checkbox);
+    checkbox.type = "checkbox";
+    checkbox.checked = completed;
+
+    checkbox.id = id;
+    checkbox.onchange = done;
+
+    if (checkbox.checked == true) {
+      li.style.textDecoration = "line-through";
+      checkbox.classList.add("tod");
+    }
   });
-  // Reset the input value to none and keep count of todos in the list
-  input.value = "";
-  h2.textContent = `TODOS (${li.length})`;
-  // displaying a clear all button + short note if ther's no todos
+  // Clear INPUT after added TODO
+  inputTodo.value = "";
+  // TOGGLE BUTTON AND MESSAGE DEPENDING HOW MANY TODOS ARE IN THE ARRAY
   if (li.length == 0) {
     displayDefault.style.display = "block";
     deleteAll.style.display = "none";
@@ -138,5 +132,6 @@ const render = () => {
     displayDefault.style.display = "none";
     deleteAll.style.display = "block";
   }
+  countTasks();
 };
 render();
